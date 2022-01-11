b416a9fd289c1e5faef74ec933ef637eb5c69b13
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-01-11 10:19:05 +0100

    Add files via upload

---
+++ trackBar/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace trackBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.txt_Ispis = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Postavljena vrijednost je:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // trackBar
+            // 
+            this.trackBar.LargeChange = 20;
+            this.trackBar.Location = new System.Drawing.Point(60, 33);
+            this.trackBar.Maximum = 100;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(299, 45);
+            this.trackBar.TabIndex = 2;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // txt_Ispis
+            // 
+            this.txt_Ispis.Location = new System.Drawing.Point(199, 136);
+            this.txt_Ispis.Name = "txt_Ispis";
+            this.txt_Ispis.Size = new System.Drawing.Size(29, 20);
+            this.txt_Ispis.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(395, 244);
+            this.Controls.Add(this.txt_Ispis);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIspis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trckVolume;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.TextBox txt_Ispis;
+    }
+}
+
